cf28236f06d742ae8d52f929f33f1934c9e77d59
Alex Sun <alex.sun@oocl.com>
2016-07-27 13:53:40 +0800

    nqueen

--- src/main/java/com/chaos/leetcode/basic/NQueens.java
+++ src/main/java/com/chaos/leetcode/basic/NQueens.java
@@ -7,16 +7,16 @@ public class NQueens {
 
     private int[] occupied; //occupied[1] = 2 means (1, 2) is occupied. 0 is not used.
 
-    private int numOfSolution;
+    private long numOfSolution;
 
     private boolean needPrint = true;
 
     public NQueens(int n) {
-        occupied = new int[n + 1];
+        occupied = new int[n];
     }
 
     private boolean available(int x, int y) {
-        for (int i = 1; i < x; i++) {
+        for (int i = 0; i < x; i++) {
             if (occupied[i] == y) {
                 return false;
             }
@@ -39,7 +39,7 @@ public class NQueens {
             return;
         }
 
-        for (int j = 1; j < occupied.length; j++) {
+        for (int j = 0; j < occupied.length; j++) {
             if (!available(i, j)) {
                 continue;
             }
@@ -52,20 +52,20 @@ public class NQueens {
     private void printSolution() {
         System.out.println("Solution " + numOfSolution + ":");
 
-        for (int y = 1; y < occupied.length; y++) {
-            for (int x = 1; x < occupied.length; x++) {
+        for (int y = 0; y < occupied.length; y++) {
+            for (int x = 0; x < occupied.length; x++) {
                 System.out.print(occupied[x] == y ? "Q " : "_ ");
             }
             System.out.println();
         }
 
-        for (int y = 1; y < occupied.length; y++) {
+        for (int y = 0; y < occupied.length; y++) {
             System.out.print("==");
         }
         System.out.println();
     }
 
-    public int getNumOfSolution() {
+    public long getNumOfSolution() {
         return numOfSolution;
     }
 
@@ -74,9 +74,9 @@ public class NQueens {
     }
 
     public static void main(String[] args) {
-        NQueens queen = new NQueens(13);
-        queen.setNeedPrint(false);
-        queen.backtracking(1);
+        NQueens queen = new NQueens(8);
+        //queen.setNeedPrint(false);
+        queen.backtracking(0);
         System.out.println("total solution:" + queen.getNumOfSolution());
     }
 
